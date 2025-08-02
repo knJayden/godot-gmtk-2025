f35efbb869d7a613f68d61cd9cf1504e2c845833
Ashton <martinezashton03@gmail.com>
2025-08-02 10:38:34 -0400

    Add bubble

--- Player/character_movement.gd
+++ Player/character_movement.gd
@@ -4,28 +4,93 @@ extends CharacterBody2D
 @export var gravity: float
 @export var acceleration: int
 @export var friction: float
+@export var max_move_velocity: float
+
+@export var bubble_duration: float
+ 
+var bubble_cooldown_timer: Timer
+
+var bubble_duration_timer: Timer
+
+
+var bubble_on_cooldown: bool = false
+
+var bubble_active: bool = false
+
+var bubble_sprite: Sprite2D
+
+func _ready():
+	bubble_cooldown_timer = $BubbleCooldown
+	bubble_duration_timer = $BubbleDuration
+	bubble_sprite = $BubbleSprite
+
 
 func _input(event):
 	if event.is_action_pressed("jump") and is_on_floor():
-		print("jump")
 		velocity.y = -jump_force
+	
+	if event.is_action_pressed("bubble") and !bubble_on_cooldown:
+		print("Activate bubble")
+		bubble_sprite.visible = true
+		bubble_active = true
+		bubble_on_cooldown = true
+		bubble_duration_timer.start(bubble_duration)
 
+	
 func _physics_process(delta):
-	if !is_on_floor():
-		velocity.y += gravity
-	else:
+	
+	apply_gravity(delta)
+	
+	var is_input = Input.is_action_pressed("right") or Input.is_action_pressed("left")
+		
+	if is_on_floor() and !is_input:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
 
-	# Handle input
+
 	if Input.is_action_pressed("right"):
 		velocity.x += acceleration * delta
 		
 	if Input.is_action_pressed("left"):
 		velocity.x -= acceleration * delta
-
+	
+	
+	if abs(velocity.x) > max_move_velocity:
+		velocity.x = max_move_velocity
+	
 	move_and_slide()
 
 func _on_hit_box_area_entered(area):
 	if area is HurtBox:
-		var vector = position - area.position
-		velocity = -(vector * 1)
+		if bubble_active:
+			var collision_normal = (global_position - area.global_position).normalized()
+
+			var min_bounce_strength = 400
+			var current_speed = velocity.length()
+			var bounce_speed = max(current_speed, min_bounce_strength)
+
+			velocity = velocity.bounce(collision_normal).normalized() * bounce_speed
+
+
+
+		else:
+			print("Dead")
+
+
+func apply_gravity(delta: float):
+	if !is_on_floor():
+		velocity.y += gravity
+
+
+
+
+func _on_bubble_duration_timeout():
+	print("Bubble over")
+	bubble_sprite.visible = false
+
+	bubble_active = false
+	bubble_cooldown_timer.start(2)
+
+
+func _on_bubble_cooldown_timeout():
+	print("Bubble ready")
+	bubble_on_cooldown = false
