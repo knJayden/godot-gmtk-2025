2aad6180bd317de6bcf6708e92259f6e061985f3
Ashton <martinezashton03@gmail.com>
2025-08-01 20:28:44 -0400

    Add obstacle, edit player movement

--- Player/character_movement.gd
+++ Player/character_movement.gd
@@ -2,22 +2,30 @@ extends CharacterBody2D
 
 @export var jump_force: int
 @export var gravity: float
-@export var speed: int
+@export var acceleration: int
+@export var friction: float
 
 func _input(event):
 	if event.is_action_pressed("jump") and is_on_floor():
-		velocity.y = -speed
-	
+		print("jump")
+		velocity.y = -jump_force
 
 func _physics_process(delta):
 	if !is_on_floor():
 		velocity.y += gravity
-	
-	
+	else:
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+	# Handle input
 	if Input.is_action_pressed("right"):
-		position.x += speed * delta
+		velocity.x += acceleration * delta
 		
 	if Input.is_action_pressed("left"):
-		position.x -= speed * delta
+		velocity.x -= acceleration * delta
 
 	move_and_slide()
+
+func _on_hit_box_area_entered(area):
+	if area is HurtBox:
+		var vector = position - area.position
+		velocity = -(vector * 1)
